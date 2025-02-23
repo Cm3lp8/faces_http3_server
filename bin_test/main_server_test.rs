1a086887b5c35c465726fcb85e1045ecf961200f
Camille Le Pennec <camille.lpennec@gmail.com>
2025-02-24 00:42:25 +0100

    reduction on numbers of packets written on socket before polling

--- bin_test/main_server_test.rs
+++ bin_test/main_server_test.rs
@@ -43,8 +43,48 @@ fn main() {
         })
         .set_request_type(RequestType::Ping)
         .build();
+    let request_form_1 = RequestForm::new()
+        .set_path("/large_data")
+        .set_method(H3Method::GET)
+        .set_scheme("https")
+        .set_body_callback(|path, args| {
+            /*
+                        let mut name: Option<&str> = None;
+                        if let Some(args) = args {
+                            args.iter().find(|item| {
+                                if let Some((field, value)) = item.split_once("=") {
+                                    if field == "name" {
+                                        name = Some(value);
+                                    }
+                                    true
+                                } else {
+                                    false
+                                }
+                            });
+                        }
+
+                        let mut body: Vec<u8> = vec![];
+                        if let Some(name) = name {
+                            body = format!(
+                                "Hello {}, I made a dream last night! However, It wasn't about Tibet",
+                                name
+                            )
+                            .as_str()
+                            .as_bytes()
+                            .to_vec();
+                            return Ok((body, b"text/plain".to_vec()));
+                        }
+                        body =
+                            b"Hello unknown person, I made a dream last night! However, It wasn't about Tibet"
+                                .to_vec();
+            */
+            Ok((vec![2; 2_400_000_000], b"text/plain".to_vec()))
+        })
+        .set_request_type(RequestType::Ping)
+        .build();
 
     request_manager.add_new_request_form(request_form_0);
+    request_manager.add_new_request_form(request_form_1);
 
     let server_config = ServerConfig::new()
         .set_address(addr)
