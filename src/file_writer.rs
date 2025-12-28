b76362c62b757cf99660ba5641d03eeb1efc6d41
Camille Le Pennec <camille.lpennec@gmail.com>
2025-12-28 20:15:10 +0100

    fileWriterhandleR3

--- src/file_writer.rs
+++ src/file_writer.rs
@@ -148,10 +148,10 @@ mod writable_type {
     }
 
     impl FileWriterHandle<std::fs::File> {
+        /// [`on_file_written`] trigs a callback when required content is written .
         pub fn on_file_written(
             &mut self,
             content_length_required: usize,
-            bytes_to_read: usize,
             cb: impl FnOnce(usize) + Send + Sync + 'static,
         ) {
             let file_h = self.inner.clone();
