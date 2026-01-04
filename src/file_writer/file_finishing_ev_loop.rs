4055ed0e22ea22a058e34ca020b7ca36d3253206
Camille Le Pennec <camille.lpennec@gmail.com>
2026-01-04 19:05:35 +0100

    14

--- src/file_writer/file_finishing_ev_loop.rs
+++ src/file_writer/file_finishing_ev_loop.rs
@@ -1,6 +1,9 @@
 use super::event_loop_types::FileFinishingEvEvent;
 pub fn manage_file_finishing_ev(event: FileFinishingEvEvent, context: &()) {
     match event {
-        FileFinishingEvEvent::FinishingFileWrite { cb } => (cb)(),
+        FileFinishingEvEvent::FinishingFileWrite { cb } => {
+            warn!("FileFinishingEvEvent received !!!");
+            (cb)()
+        }
     }
 }
